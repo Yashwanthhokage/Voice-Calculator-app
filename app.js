3d970fb6fa2cd9f40df57bc336d869f354fa824d
Yashwanthhokage <107128663+Yashwanthhokage@users.noreply.github.com>
2026-01-30 22:19:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -155,7 +155,6 @@ recognition.onresult = function(event) {
   
   const transcript = event.results[0][0].transcript;
   micBtn.classList.remove("listening");
-  alert("VOICE HEARD: " + transcript);
   calculateFromVoiceAI(transcript);
   
   // ✅ Unlock after 1 second
